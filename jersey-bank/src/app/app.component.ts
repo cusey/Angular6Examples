b68feb31d153283de7eaf6f06dd5311acb1459cb
joe99peace <jjcusey@gmail.com>
2019-02-24 17:19:25 -0500

    Branch Location Components
    Added the Location Component with navigation and routing

--- jersey-bank/src/app/app.component.ts
+++ jersey-bank/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +10,18 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class AppComponent {
   title = 'Jersey National Bank';
+
+  constructor(private router: Router) { }
+
+  signIn() {
+
+  }
+
+  signUp() {
+    this.router.navigate(['/sign-up'], {
+        queryParams: { type: 'New'}
+      }
+    );
+  }
+
 }
--- jersey-bank/src/app/app.module.ts
+++ jersey-bank/src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LocationsComponent } from './locations/locations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LocationsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: 'app-locations',
+        component: LocationsComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
